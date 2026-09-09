22dde8c738f991009ff5c9e3a2e079a12089c889
matheus15-hub <matheus.omega15@gmail.com>
2026-09-09 16:08:53 -0300

    atividade 3: criando 7 rotas no AlunoController

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,44 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    // Listar todos os alunos
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+
+    public function show($id)
+    {
+        return 'Aluno: ' . $id;
+    }
+
+   
+    public function create()
+    {
+        return 'Formulário para criar aluno';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno criado';
+    }
+
+
+    public function edit($id)
+    {
+        return 'Formulário para editar aluno: ' . $id;
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        return 'Aluno atualizado: ' . $id;
+    }
+
+    public function destroy($id)
+    {
+        return 'Aluno excluído: ' . $id;
+    }
+
 }
