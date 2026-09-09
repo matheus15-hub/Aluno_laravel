c9dd777a5dc66739aff13c10b98367360e96aa09
matheus15-hub <matheus.omega15@gmail.com>
2026-09-09 16:40:04 -0300

    atividade 7: criar layout principal

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>@yield('title', 'Sistema de Alunos')</title>
+</head>
+<body>
+
+    <header>
+        <h1>Sistema de Alunos</h1>
+
+        <nav>
+            <a href="/alunos">Alunos</a>
+            <a href="/sobre">Sobre</a>
+            <a href="/contato">Contato</a>
+        </nav>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+
+</body>
+</html>
