156922153361ce6cbc823c7c1f88bbfca79a2c3d
matheus15-hub <matheus.omega15@gmail.com>
2026-09-09 15:50:20 -0300

    atividade 1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sobre', function () {
+    return 'Página sobre';
+});
+
+Route::get('/alunos', function () {
+    return 'Página de alunos';
+});
+
+Route::get('/contato', function () {
+    return 'Página de contato';
+});
